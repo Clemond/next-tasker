7bd1adaf0ded50062da8fa31d8d3b2b581c1ec7c
Nicholas <quackshows11@gmail.com>
2024-12-12 17:57:04 +0100

    style: added background picture, added 'Today's task' and date  paragrafs

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
 
 export default function Home() {
+  const date = new Date().toLocaleDateString("en-GB", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
   return (
-    <div>
-      <p>Hello world</p>
+    <div
+      className="h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url('/todo-background.jpg')" }}
+    >
+      <div className="text-white text-2xl font-bold p-5">
+        <p>Today's Tasks</p>
+        <p className="font-thin">{date}</p>
+      </div>
     </div>
   );
 }
