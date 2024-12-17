253f0a0d00cb6f82cef2961044b6348e18c0370d
Nicholas <quackshows11@gmail.com>
2024-12-17 15:05:29 +0100

    add:  user registration logic to frontend

--- app/(authentication)/signup/page.tsx
+++ app/(authentication)/signup/page.tsx
@@ -1,4 +1,46 @@
+"use client";
+import { CustomUser } from "@/app/_type/ICustomUser";
+import { ChangeEvent, FormEvent, useState } from "react";
+
 export default function SignUp() {
+  const [customUser, setCustomUser] = useState<CustomUser>({
+    username: "",
+    password: "",
+  });
+
+  async function handleOnSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const newUser: CustomUser = {
+      username: customUser.username,
+      password: customUser.password,
+    };
+
+    // POST
+    const result = await fetch("http://localhost:8080/api/v1/user", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+      },
+      body: JSON.stringify(newUser),
+    });
+
+    if (result.ok) {
+      const data = await result.text();
+      console.log(data);
+    } else {
+      const resultError = await result.json();
+      console.error(resultError);
+    }
+  }
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setCustomUser((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  }
+
   return (
     <div
       className="h-screen bg-cover bg-center"
@@ -7,7 +49,7 @@ export default function SignUp() {
       <div className="h-screen flex flex-col items-center justify-center">
         <p className="p-4 text-xl font-bold">Sign up!</p>
         <form
-          //onSubmit={}
+          onSubmit={handleOnSubmit}
           method="post"
           className="flex flex-col max-w-sm gap-4"
         >
@@ -17,8 +59,8 @@ export default function SignUp() {
             className="text-black"
             type="text"
             name="username"
-            //value={customUser.username}
-            //onChange={handleChange}
+            value={customUser.username}
+            onChange={handleChange}
           />
           {/* Password */}
           <label htmlFor="password">Password</label>
@@ -26,8 +68,8 @@ export default function SignUp() {
             className="text-black"
             type="password"
             name="password"
-            //value={customUser.password}
-            //onChange={handleChange}
+            value={customUser.password}
+            onChange={handleChange}
           />
 
           <button
---
+++ app/_type/ICustomUser.ts
@@ -0,0 +1,4 @@
+export interface CustomUser {
+  username: string;
+  password: string;
+}
